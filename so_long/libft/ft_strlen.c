c7664ee4bca49cfc6aa64fb52ea2bfc547cbb816
Younes Anflous <anayouness51@gmail.com>
2025-02-03 14:04:09 +0100

    strlen

--- so_long/libft/ft_strlen.c
+++ so_long/libft/ft_strlen.c
@@ -1,23 +1,11 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yanflous <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/02 14:28:13 by yanflous          #+#    #+#             */
-/*   Updated: 2025/02/02 14:28:58 by yanflous         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "../so_long/so_long.h"
+#include "../src_so_long/so_long.h"
 
 size_t	ft_strlen(char *str)
 {
-	size_t	len;
+	size_t	s_len;
 
-	len = 0;
-	while (str[len])
-		len++;
-	return (len);
+	s_len = 0;
+	while (str[s_len])
+		s_len++;
+	return (s_len);
 }
