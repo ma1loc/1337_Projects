81eb20ad9bac9cdd8bae03ab8568356fca7149f3
Younes Anflous <anayouness51@gmail.com>
2025-02-19 13:44:37 +0100

    render the map

--- so_long/map_rendring/render_the_map.c
+++ so_long/map_rendring/render_the_map.c
@@ -1,18 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render_the_map.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yanflous <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/19 13:01:33 by yanflous          #+#    #+#             */
+/*   Updated: 2025/02/19 13:01:35 by yanflous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../src_so_long/so_long.h"
 
 void	init_and_window(t_game *game)
 {
-	int		height;
-	int 	width;
-	
+	int	height;
+	int	width;
+
 	game->mlx = mlx_init();
 	if (!game->mlx)
-		cleanup_and_exit("Error\nnInitialization of the mlx failedn\n", game, 1);
+		cleanup_and_exit("Error\nInitialization of the mlx failedn\n", game, 2);
 	width = game->cols * 64;
 	height = game->rows * 64;
 	game->win = mlx_new_window(game->mlx, width, height, "so_long");
 	if (!game->win)
-		cleanup_and_exit("Error\nWindow failed to open.", game, 1);
+		cleanup_and_exit("Error\nWindow failed to open.", game, 2);
 }
 
 void	load_img_to_win(t_game *game)
@@ -24,11 +36,11 @@ void	load_img_to_win(t_game *game)
 	init_the_map(game);
 }
 
-void process_the_map_rendering(t_game *game)
+void	process_the_map_rendering(t_game *game)
 {
 	game->collected = 0;
-    init_and_window(game);
-    load_img_to_win(game);
-    mlx_key_hook(game->win, press_key, game);
-    mlx_loop(game->mlx);
+	init_and_window(game);
+	load_img_to_win(game);
+	mlx_key_hook(game->win, press_key, game);
+	mlx_loop(game->mlx);
 }
