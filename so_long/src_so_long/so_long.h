9991b0cced43bd95737cb413b55dc0d15f381666
Younes Anflous <anayouness51@gmail.com>
2025-02-19 13:45:25 +0100

    header file

--- so_long/src_so_long/so_long.h
+++ so_long/src_so_long/so_long.h
@@ -1,27 +1,39 @@
-# ifndef SO_LONG_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yanflous <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/19 10:42:50 by yanflous          #+#    #+#             */
+/*   Updated: 2025/02/19 10:42:52 by yanflous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
 # define SO_LONG_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE   42
 # endif
 
-#define UP_KEY XK_Up
-#define DOWN_KEY XK_Down
-#define LEFT_KEY XK_Left
-#define RIGHT_KEY XK_Right
-#define ESC_KEY XK_Escape
+# define UP_KEY XK_Up
+# define DOWN_KEY XK_Down
+# define LEFT_KEY XK_Left
+# define RIGHT_KEY XK_Right
+# define ESC_KEY XK_Escape
 
 # include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
 # include <stdio.h>
 # include <mlx.h>
-#include <X11/keysym.h>
+# include <X11/keysym.h>
 
 size_t	ft_strlen(char *str);
 int		ft_strncmp(char *s1, char *s2, size_t n);
 char	*ft_strnstr(char *str, char *to_find, size_t len);
-void	ft_putstr_fd(char *str);
+void	ft_putstr_fd(char *str, int std);
 char	*get_next_line(int fd);
 char	*ft_strchr(const char *str, char c);
 char	*ft_strdup(const char *str);
@@ -48,26 +60,25 @@ typedef struct s_game
 	int			moves;
 	int			player_count;
 	int			exit_count;
-	void		*mlx;			// -- done free
-	void		*win;			// -- done free
-	void		*wall;			// -- done
-	void		*coin;			// -- done
-	void		*free_space;	// -- done
-	void		*player[6];		// -- done
-	void		*exit_door[3];	// -- done
+	void		*mlx;
+	void		*win;
+	void		*wall;
+	void		*coin;
+	void		*free_space;
+	void		*player[6];
+	void		*exit_door[3];
 }	t_game;
 
 typedef struct s_direction
 {
-	int new_row;
-	int new_col;
-} t_direction;
-
+	int	new_row;
+	int	new_col;
+}	t_direction;
 
 t_game	*pars_the_map(char *map);
 t_game	*map_processing(char *map);
 int		count_lines(char *map);
-char    **read_map(char *map);
+char	**read_map(char *map);
 void	is_map_valid(t_game *game);
 void	wall_check(t_game *game);
 void	cleanup_and_exit(char *str, t_game *game, int msg);
