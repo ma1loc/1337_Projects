c19f2b65e734cbbddfe2133791a1ce48aad472e8
Younes Anflous <anayouness51@gmail.com>
2025-02-19 13:43:34 +0100

    load img

--- so_long/map_rendring/load_images.c
+++ so_long/map_rendring/load_images.c
@@ -1,16 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   load_images.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yanflous <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/19 11:44:38 by yanflous          #+#    #+#             */
+/*   Updated: 2025/02/19 11:48:14 by yanflous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../src_so_long/so_long.h"
 
 void	load_player_images(t_game *game)
 {
-	int	width = 0;
-	int	height = 0;
+	int	width;
+	int	height;
 
 	game->player[0] = mlx_xpm_file_to_image(game->mlx, \
 		"map_rendring/img_resource/player/defult.xpm", \
-	&	width, &height);
+		&width, &height);
 	game->player[1] = mlx_xpm_file_to_image(game->mlx, \
 		"map_rendring/img_resource/player/up.xpm", \
-	&	width, &height);
+		&width, &height);
 	game->player[2] = mlx_xpm_file_to_image(game->mlx, \
 		"map_rendring/img_resource/player/down.xpm", \
 		&width, &height);
@@ -23,20 +35,21 @@ void	load_player_images(t_game *game)
 	game->player[5] = NULL;
 	if (!game->player[0] || !game->player[1] || !game->player[2] \
 		|| !game->player[3] || !game->player[4])
-		cleanup_and_exit("Error: Failed to load player images\n", game, 1);
+		cleanup_and_exit("Error\nFailed to load player images\n", game, 2);
 }
 
-void load_space_and_wall_images(t_game *game)
+void	load_space_and_wall_images(t_game *game)
 {
 	int	width;
 	int	height;
-	
-	game->free_space = mlx_xpm_file_to_image(game->mlx,\
+
+	game->free_space = mlx_xpm_file_to_image(game->mlx, \
 		"map_rendring/img_resource/background/space.xpm", &width, &height);
-	game->wall = mlx_xpm_file_to_image(game->mlx,\
+	game->wall = mlx_xpm_file_to_image(game->mlx, \
 		"map_rendring/img_resource/wall/wall.xpm", &width, &height);
 	if (!game->free_space || !game->wall)
-		cleanup_and_exit("Error: Failed to load free_sapce or wall images\n", game, 1);
+		cleanup_and_exit("Error\n\
+		Failed to load free_sapce or wall images\n", game, 2);
 }
 
 void	load_doors_images(t_game *game)
@@ -44,22 +57,22 @@ void	load_doors_images(t_game *game)
 	int	width;
 	int	height;
 
-	game->exit_door[0] = mlx_xpm_file_to_image(game->mlx,\
+	game->exit_door[0] = mlx_xpm_file_to_image(game->mlx, \
 		"map_rendring/img_resource/door/door_close.xpm", &width, &height);
-	game->exit_door[1] = mlx_xpm_file_to_image(game->mlx,\
+	game->exit_door[1] = mlx_xpm_file_to_image(game->mlx, \
 		"map_rendring/img_resource/door/door_open.xpm", &width, &height);
 	game->exit_door[2] = NULL;
 	if (!game->exit_door[0] || !game->exit_door[1])
-		cleanup_and_exit("Error: Failed to load doors images\n", game, 1);
+		cleanup_and_exit("Error\nFailed to load doors images\n", game, 2);
 }
 
-void load_coin_images(t_game *game)
+void	load_coin_images(t_game *game)
 {
-	int width;
-	int height;
+	int	width;
+	int	height;
 
-	game->coin = mlx_xpm_file_to_image(game->mlx,\
+	game->coin = mlx_xpm_file_to_image(game->mlx, \
 		"map_rendring/img_resource/coin/coin.xpm", &width, &height);
 	if (!game->coin)
-		cleanup_and_exit("Error: Failed to load coins images\n", game, 1);
+		cleanup_and_exit("Error\nFailed to load coins images\n", game, 2);
 }
