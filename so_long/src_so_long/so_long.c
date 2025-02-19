740b21ae98cca18dca57855b4f331459b5c69504
Younes Anflous <anayouness51@gmail.com>
2025-02-19 13:45:44 +0100

    so_long

--- so_long/src_so_long/so_long.c
+++ so_long/src_so_long/so_long.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yanflous <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/19 10:36:40 by yanflous          #+#    #+#             */
+/*   Updated: 2025/02/19 10:36:43 by yanflous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "so_long.h"
 
 int	main(int argc, char **argv)
@@ -6,7 +18,7 @@ int	main(int argc, char **argv)
 
 	game = NULL;
 	if (argc < 2)
-		ft_putstr_fd("Error\nA few args, put the map to play\n");
+		ft_putstr_fd("Error\nA few args, put the map to play\n", 2);
 	if (argc == 2)
 	{
 		game = pars_the_map(argv[1]);
@@ -14,7 +26,7 @@ int	main(int argc, char **argv)
 	}
 	else
 		ft_putstr_fd("Error\nToo many arguments.\
-		\nUsage => \"./so_long map.ber\"");
+		\nUsage => \"./so_long map.ber\"", 2);
 	map_free(game->map);
 	map_free(game->map_copy);
 	free(game);
