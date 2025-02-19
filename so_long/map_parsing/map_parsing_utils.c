f28758729a75f39564da942a59f68921dd4a521a
Younes Anflous <anayouness51@gmail.com>
2025-02-19 13:42:19 +0100

    parse utils

--- so_long/map_parsing/map_parsing_utils.c
+++ so_long/map_parsing/map_parsing_utils.c
@@ -1,18 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_parsing_utils.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yanflous <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/19 11:13:14 by yanflous          #+#    #+#             */
+/*   Updated: 2025/02/19 11:15:16 by yanflous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../src_so_long/so_long.h"
 
 int	count_lines(char *map)
 {
-    int		fd;
-    int		count;
-    char	*line;
+	int		fd;
+	int		count;
+	char	*line;
 
-    count = 0;
-    fd = open(map, O_RDONLY);
-    if (fd == -1)
-        ft_perror("Error\nOpen");
-    line = get_next_line(fd);
-    while (line)
-    {
+	count = 0;
+	fd = open(map, O_RDONLY);
+	if (fd == -1)
+		ft_perror("Error\nOpen");
+	line = get_next_line(fd);
+	while (line)
+	{
 		count++;
 		free(line);
 		line = get_next_line(fd);
@@ -21,15 +33,15 @@ int	count_lines(char *map)
 	return (count);
 }
 
-char    **read_map(char *map)
+char	**read_map(char *map)
 {
-    char    *line;
-    char    **split_map;
-    int     i;
-    int     fd;
+	char	*line;
+	char	**split_map;
+	int		i;
+	int		fd;
 
 	if (!map)
-        return (NULL);
+		return (NULL);
 	split_map = malloc(sizeof(char *) * (count_lines(map) + 1));
 	if (!split_map)
 		return (NULL);
