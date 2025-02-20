9060db1f2293d8b1244e2266c83e27e6b57ca60b
Younes Anflous <anayouness51@gmail.com>
2025-02-20 18:20:01 +0100

    valid file func

--- so_long/map_parsing/is_valid_file.c
+++ so_long/map_parsing/is_valid_file.c
@@ -6,7 +6,7 @@
 /*   By: yanflous <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 10:57:38 by yanflous          #+#    #+#             */
-/*   Updated: 2025/02/19 10:57:42 by yanflous         ###   ########.fr       */
+/*   Updated: 2025/02/20 13:24:41 by yanflous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,18 +33,42 @@ void	is_map_valid(t_game *game)
 	if (game->map[i])
 		len_line = ft_strlen(game->map[i]);
 	else
-		cleanup_and_exit("Error\nMap is empty.", game, 2);
+		cleanup_and_exit("Error\nMap is empty.", game, 2, 0);
 	while (game->map[i])
 	{
 		if (game->map[i][0] == '\n' && game->map[i][1] == '\0')
-			cleanup_and_exit("Error\nProvided map has empty line.", game, 2);
+			cleanup_and_exit("Error\nProvided map has empty line.", game, 2, 0);
 		line = game->map[i];
 		if (line[len_line - 1] != '\n')
 			len_line--;
 		if (ft_strlen(line) != len_line)
-			cleanup_and_exit("Error\nMap is not \"Rectangular\".", game, 2);
+			cleanup_and_exit("Error\nMap is not \"Rectangular\".", game, 2, 0);
 		i++;
 	}
+}
+
+void	game_init(t_game *game)
+{
+	int	i;
+
+	game->player_count = 0;
+	game->moves = 0;
+	game->exit_count = 0;
+	game->collectibles = 0;
+	game->collected = 0;
+	game->map = NULL;
+	game->map_copy = NULL;
+	game->win = NULL;
+	game->wall = NULL;
+	game->coin = NULL;
+	game->free_space = NULL;
+	game->mlx = NULL;
+	i = 0;
+	while (i < 6)
+		game->player[i++] = NULL;
+	i = 0;
+	while (i < 3)
+		game->exit_door[i++] = NULL;
 }
 
 t_game	*map_processing(char *map)
@@ -54,13 +78,12 @@ t_game	*map_processing(char *map)
 	game = malloc(sizeof(t_game));
 	if (!game)
 		ft_putstr_fd("Error\nAllocation faild.", 2);
+	game_init(game);
 	game->map = read_map(map);
 	if (!game->map)
-		cleanup_and_exit("Error\nParsing Faild!!!", game, 2);
-	game->exit_count = 0;
-	game->collected = 0;
+		cleanup_and_exit("Error\nParsing Faild!!!", game, 2, 0);
 	is_map_valid(game);
-	count_duplicate_char_in_the_map(game); // here the seg
+	count_duplicate_char_in_the_map(game);
 	check_is_valid_dup_char(game);
 	wall_check(game);
 	game->rows = count_lines(map);
