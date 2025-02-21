43cb2d09b2c5303dafe59ecfc64686c00c4258a5
Younes Anflous <anayouness51@gmail.com>
2025-02-21 14:20:45 +0100

    valid file func

--- so_long/map_parsing/is_valid_file.c
+++ so_long/map_parsing/is_valid_file.c
@@ -20,55 +20,47 @@ void	is_there_extension(char *map)
 	if (len < 4 || ft_strncmp(&map[len - 4], ".ber", 4) != 0
 		|| (ft_strncmp(map, ".ber", 4) == 0))
 		ft_putstr_fd("Error\nThe extenstion not valid.\
-		\nUsage => \"./so_long *.ber\".", 2);
+		\nUsage => \"./so_long *.ber\".\n", 2);
 }
 
-void	is_map_valid(t_game *game)
+void	edges_loop(t_game *game, size_t _line_len)
 {
-	size_t	len_line;
+	size_t	line_len;
 	char	*line;
 	int		i;
 
 	i = 0;
-	if (game->map[i])
-		len_line = ft_strlen(game->map[i]);
-	else
-		cleanup_and_exit("Error\nMap is empty.", game, 2, 0);
 	while (game->map[i])
 	{
-		if (game->map[i][0] == '\n' && game->map[i][1] == '\0')
-			cleanup_and_exit("Error\nProvided map has empty line.", game, 2, 0);
 		line = game->map[i];
-		if (line[len_line - 1] != '\n')
-			len_line--;
-		if (ft_strlen(line) != len_line)
-			cleanup_and_exit("Error\nMap is not \"Rectangular\".", game, 2, 0);
+		if (line[0] == '\n' && line[1] == '\0')
+			cleanup_and_exit("Error\n" \
+			"Provided map has empty line.\n", game, 2, 0);
+		line_len = ft_strlen(line);
+		if (line_len > 0 && line[line_len - 1] == '\n')
+			line_len--;
+		if (line_len != _line_len)
+			cleanup_and_exit("Error\n" \
+			"Map is not \"Rectangular\".\n", game, 2, 0);
 		i++;
 	}
 }
 
-void	game_init(t_game *game)
+void	is_map_valid(t_game *game)
 {
-	int	i;
+	size_t	line_len;
+	int		i;
 
-	game->player_count = 0;
-	game->moves = 0;
-	game->exit_count = 0;
-	game->collectibles = 0;
-	game->collected = 0;
-	game->map = NULL;
-	game->map_copy = NULL;
-	game->win = NULL;
-	game->wall = NULL;
-	game->coin = NULL;
-	game->free_space = NULL;
-	game->mlx = NULL;
 	i = 0;
-	while (i < 6)
-		game->player[i++] = NULL;
-	i = 0;
-	while (i < 3)
-		game->exit_door[i++] = NULL;
+	line_len = ft_strlen(game->map[i]);
+	if (game->map[i])
+	{
+		if (game->map[i][line_len - 1] == '\n')
+			line_len--;
+	}
+	else
+		cleanup_and_exit("Error\nMap is empty.\n", game, 2, 0);
+	edges_loop(game, line_len);
 }
 
 t_game	*map_processing(char *map)
@@ -77,20 +69,20 @@ t_game	*map_processing(char *map)
 
 	game = malloc(sizeof(t_game));
 	if (!game)
-		ft_putstr_fd("Error\nAllocation faild.", 2);
+		ft_putstr_fd("Error\nAllocation faild.\n", 2);
 	game_init(game);
-	game->map = read_map(map);
+	game->map = read_map(map, game);
 	if (!game->map)
-		cleanup_and_exit("Error\nParsing Faild!!!", game, 2, 0);
+		cleanup_and_exit("Error\nParsing Faild!!!\n", game, 2, 0);
 	is_map_valid(game);
 	count_duplicate_char_in_the_map(game);
 	check_is_valid_dup_char(game);
 	wall_check(game);
-	game->rows = count_lines(map);
+	game->rows = count_lines(map, game);
 	game->cols = count_cols(game->map);
 	finding_player_position(game);
 	finding_player_exit(game);
-	game->map_copy = read_map(map);
+	game->map_copy = read_map(map, game);
 	return (game);
 }
 
